c69aa9ea5f8b7c4e69a00c87d268ab3076f04d09
mjkaufer <mjkaufer@gmail.com>
2015-04-10 23:19:25 -0400

    Basic packages, some functionality

--- client/client.js
+++ client/client.js
@@ -1,5 +1,17 @@
 Template.controls.events({
   'click #ai': function () {
     //run diagnostics
+
+    var subreddit = $('#subreddit').val();
+
+    reddit.top(subreddit)
+      .limit(100)
+      .t('month')
+      .fetch(function(res){
+
+        console.log(res)
+
+      });
+
   }
 });
